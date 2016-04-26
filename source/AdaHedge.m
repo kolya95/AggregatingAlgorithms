f1dc1685237815859089640a17022c55473092f1
kolya95 <nikolaypopov95@gmail.com>
2016-04-26 08:39:51 +0300

    renamed
    renamed adahedgefixshare -> adahedge

--- source/AdaHedge.m
+++ source/AdaHedge.m
@@ -1,15 +1,15 @@
-function h = AdaHedgeFixShare(l, alpha)
+function h = AdaHedge(l)
     [T, K] = size(l);
     h = nan(T,1);
     L = zeros(1, K);
     Delta = 0;
     for t = 1:T
         eta = max(log(K), 1)/Delta;
-        [w, Mprev] = mix(eta, L, alpha);
+        [w, Mprev] = mix(eta, L);
         
         h(t) = w * l(t, :)';
         L = L + l(t, :);
-        [~, M] = mix(eta, L, alpha);
+        [~, M] = mix(eta, L);
         
         delta = max(0, h(t) - (M-Mprev));
         Delta = Delta + delta;
